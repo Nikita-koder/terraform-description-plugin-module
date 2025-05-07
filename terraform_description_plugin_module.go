8e59b330ec68efeb80085d46db1bf44f2d89d014
Nikita <NAfanasev@t1-cloud.ru>
2025-05-07 10:42:46 +0300

    fix: add file range

--- terraform_description_plugin_module.go
+++ terraform_description_plugin_module.go
@@ -53,46 +53,48 @@ func (f *PluginExample) GetLoadMode() string {
 }
 
 func (f *PluginExample) run(pass *analysis.Pass) (interface{}, error) {
-	ast.Inspect(pass.Files[0], func(n ast.Node) bool {
-		composite, ok := n.(*ast.CompositeLit)
-		if !ok {
-			return true
-		}
-
-		// Проверка типа: *schema.Schema
-		selExpr, ok := composite.Type.(*ast.SelectorExpr)
-		if !ok || selExpr.Sel.Name != "Schema" {
-			return true
-		}
-
-		ident, ok := selExpr.X.(*ast.Ident)
-		if !ok || ident.Name != "schema" {
-			return true
-		}
-
-		for _, elt := range composite.Elts {
-			kv, ok := elt.(*ast.KeyValueExpr)
+	for _, file := range pass.Files {
+		ast.Inspect(file, func(n ast.Node) bool {
+			composite, ok := n.(*ast.CompositeLit)
 			if !ok {
-				continue
+				return true
 			}
 
-			keyIdent, ok := kv.Key.(*ast.Ident)
-			if !ok || keyIdent.Name != "Description" {
-				continue
+			// Проверка типа: *schema.Schema
+			selExpr, ok := composite.Type.(*ast.SelectorExpr)
+			if !ok || selExpr.Sel.Name != "Schema" {
+				return true
 			}
 
-			val, ok := kv.Value.(*ast.BasicLit)
-			if !ok || val.Kind != token.STRING {
-				continue
+			ident, ok := selExpr.X.(*ast.Ident)
+			if !ok || ident.Name != "schema" {
+				return true
 			}
 
-			if val.Value == `""` {
-				pass.Reportf(val.Pos(), "Description in schema.Schema is empty")
-			}
-		}
+			for _, elt := range composite.Elts {
+				kv, ok := elt.(*ast.KeyValueExpr)
+				if !ok {
+					continue
+				}
 
-		return true
-	})
+				keyIdent, ok := kv.Key.(*ast.Ident)
+				if !ok || keyIdent.Name != "Description" {
+					continue
+				}
+
+				val, ok := kv.Value.(*ast.BasicLit)
+				if !ok || val.Kind != token.STRING {
+					continue
+				}
+
+				if val.Value == `""` {
+					pass.Reportf(val.Pos(), "Description in schema.Schema is empty")
+				}
+			}
+
+			return true
+		})
+	}
 
 	return nil, nil
 }
